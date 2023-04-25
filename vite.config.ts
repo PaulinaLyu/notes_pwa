68b8193173fc8eec995b6e3bd00c9b09b2e57f1f
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2023-04-25 12:50:49 +0300

    Попытка настроить pwa, пока ошибка

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,34 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      outDir: "dist",
+      manifest: {
+        name: "Notes React PWA",
+        short_name: "NotesPWA",
+        description: "description",
+        start_url: "/index.html",
+        display: "standalone",
+        background_color: "#fff",
+        theme_color: "#fff",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "icon/android-chrome-192x192.png",
+            size: "192x192",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "icon/android-chrome-512x512.png",
+            size: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
---
+++ workbox-config.js
@@ -0,0 +1,5 @@
+export default {
+  globDirectory: "dist/",
+  globPatterns: ["**/*/{scss,woff2,png,svg,jpg,jpeg,js,ts,tsx}"],
+  swDest: "dist/sw.js",
+};
